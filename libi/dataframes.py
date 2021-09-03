d707780ae76aa1654c9ff823a5806514af1618e5
matheus-reyes <matheuscreyes@gmail.com>
2021-09-03 10:56:23 -0300

    adding requirements and some features

--- libi/dataframes.py
+++ libi/dataframes.py
@@ -1,6 +1,22 @@
-def test_lib_external():
-    return "blah"
+import requests
+import pandas as pd
+import os
+from utils import flatten_nested_json_df
+from errors import RetrieveDataError
 
-def do_balacobaco():
-    return 'nah'
 
+BASE_URL = 'https://api.cobli.co/'
+
+
+def get_devices(api_key, current_timestamp, fleet_name=''):
+    headers = {
+        'Cobli-Api-Key': api_key,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.get(f'{BASE_URL}herbie-1.1/dash/device', headers=headers)
+
+    if response.status_code != 200:
+        raise RetrieveDataError("Não foi possível retornar os dados")
+
+    return flatten_nested_json_df(pd.DataFrame(response.json()))
